8733b73c825465d5dcd321affa144a782c356e03
mykolas.pinkevicius <mdpbeats@gmail.com>
2021-01-28 20:28:12 +0200

    remove unused RestTemplate in InvalidItemPDFReportService

--- src/main/java/lt/mykolaspinkevicius/invaliditempdfreporter/service/InvalidItemPDFReportService.java
+++ src/main/java/lt/mykolaspinkevicius/invaliditempdfreporter/service/InvalidItemPDFReportService.java
@@ -3,7 +3,6 @@ package lt.mykolaspinkevicius.invaliditempdfreporter.service;
 import lt.mykolaspinkevicius.invaliditempdfreporter.pdf.PDFReportCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
@@ -15,8 +14,6 @@ public class InvalidItemPDFReportService {
     private ItemDTOService itemDTOService;
     @Autowired
     private PDFReportCreator pdfReportCreator;
-
-    private final RestTemplate restTemplate = new RestTemplate();
 
     public ByteArrayInputStream getInvalidItems(LocalDate validDate) {
         return pdfReportCreator.preparePDF(itemDTOService.invalidItems(validDate));
