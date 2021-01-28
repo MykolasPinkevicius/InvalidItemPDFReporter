4b9439c4105364ab37ca9aea49909c7936d45ae5
mykolas.pinkevicius <mdpbeats@gmail.com>
2021-01-28 15:04:33 +0200

    Initial commit

---
+++ src/main/java/lt/mykolaspinkevicius/InvalidItemPDFReporter/InvalidItemPdfReporterApplication.java
@@ -0,0 +1,13 @@
+package lt.mykolaspinkevicius.InvalidItemPDFReporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvalidItemPdfReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvalidItemPdfReporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/mykolaspinkevicius/InvalidItemPDFReporter/InvalidItemPdfReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mykolaspinkevicius.InvalidItemPDFReporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvalidItemPdfReporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
